814eb8f406b7fdda4452db0fcc4b41ca80d175dc
annaher <31725804+annaher@users.noreply.github.com>
2018-01-30 12:57:26 +0100

    .

--- tdt4140-gr1837/app.ui/src/test/java/tdt4140/gr1837/app/ui/FxAppTest.java
+++ tdt4140-gr1837/app.ui/src/test/java/tdt4140/gr1837/app/ui/FxAppTest.java
@@ -2,7 +2,6 @@ package tdt4140.gr1837.app.ui;
 
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
