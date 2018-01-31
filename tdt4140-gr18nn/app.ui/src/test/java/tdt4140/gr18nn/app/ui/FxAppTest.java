32f5cac5b3024ea06d01e128d9381af9b6d8a863
Kewin <kevikris@gmail.com>
2018-01-31 09:09:24 +0100

    Added 18nn project, and removed the failing test in 1837 project

--- tdt4140-gr18nn/app.ui/src/test/java/tdt4140/gr18nn/app/ui/FxAppTest.java
+++ tdt4140-gr18nn/app.ui/src/test/java/tdt4140/gr18nn/app/ui/FxAppTest.java
@@ -1,29 +1,24 @@
-package tdt4140.gr1837.app.ui;
+package tdt4140.gr18nn.app.ui;
 
 import org.junit.Test;
-import org.junit.BeforeClass;
 import org.testfx.framework.junit.ApplicationTest;
-import static org.junit.Assert.*;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FxAppTest extends ApplicationTest {
-	
 
 	@Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.show(); 
+        stage.show();
     }
 
-
     @Test
-	public void test() 
-	{
-		assertTrue("Automatically successful test :)", true);
-	}
+    public void testFxApp() {
+    }
 }
