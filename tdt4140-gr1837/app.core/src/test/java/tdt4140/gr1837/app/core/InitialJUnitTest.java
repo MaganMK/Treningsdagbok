2d7a24f5b39ce0168ec92187d9381a9c33ca52ec
MaganMK <32375836+MaganMK@users.noreply.github.com>
2018-02-01 11:53:32 +0100

    Added initial JUnit test

---
+++ tdt4140-gr1837/app.core/src/test/java/tdt4140/gr1837/app/core/InitialJUnitTest.java
@@ -0,0 +1,18 @@
+package tdt4140.gr1837.app.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+//Tom JUnit-test som skal kunne være med ved bygging med Maven, men ikke teste faktisk kode.
+//(Krav til første evaluvering av prosjektet)
+public class InitialJUnitTest {
+
+	@Test
+	public void test() 
+	{
+		assertTrue("Automatically successful test  :)", true);
+	}
+
+}
