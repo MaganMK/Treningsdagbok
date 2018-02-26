6148efdca93cdbd1ad30e2aec27fc583018df6d2
MaganMK <32375836+MaganMK@users.noreply.github.com>
2018-02-26 15:16:49 +0100

    #10 Lagde klasse for muskelmannen, fikset litt på strengthtab og strengthtab-kontrolleren

---
+++ tdt4140-gr1837/app.core/src/main/java/tdt4140/gr1837/app/core/MuscleImage.java
@@ -0,0 +1,8 @@
+package tdt4140.gr1837.app.core;
+
+// Klasse for å håndtere "muskelmannen"
+public class MuscleImage {
+
+	
+	
+}
--- tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StrengthTabController.java
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StrengthTabController.java
@@ -1,8 +1,10 @@
 package tdt4140.gr1837.app.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import tdt4140.gr1837.app.core.MuscleImage;
 import tdt4140.gr1837.app.core.User;
 
 // Styrketab som viser graf for styrkeovelser samt checkboxes som velger hva som skal vises
@@ -14,14 +16,24 @@ public class StrengthTabController {
 	// Klientnavn
 	@FXML Text clientName;
 
+	//Muskelgruppene 
+	@FXML Image muscleManImage;
+	private MuscleImage muscleMan;
+	
 	// Managercontroller for kommunikasjon mellom controllers
 	public ManagerController managerController;
 	
 	// Setter managerController
 	public void init(ManagerController managerController) {
 		this.managerController = managerController;
+		setMuscleMan();
 	}
 	
+	
+	private void setMuscleMan()
+	{
+		
+	}
 	// Setter user som skrives i sokefeltet
 	public void setUser(User user) {
 			clientName.setText(user.getName());
