64a8d46d885024dce4533eef674102ed0bfe429b
annaher <31725804+annaher@users.noreply.github.com>
2018-02-27 17:12:24 +0100

    #10 Kopiert bildene til en ny mappe i app.ui pga path-trøbbel. Satt inn alle muskler, lagd liten funksjon som setter random opacity

--- tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StrengthTabController.java
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StrengthTabController.java
@@ -1,9 +1,11 @@
 package tdt4140.gr1837.app.ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -32,7 +34,17 @@ public class StrengthTabController {
 	@FXML ImageView erector;
 	@FXML ImageView calves;
 	@FXML ImageView biceps;
+	@FXML ImageView quads;
+	@FXML ImageView rumpe;
+	@FXML ImageView traps;
+	@FXML ImageView hofteUt;
+	@FXML ImageView bryst;
+	@FXML ImageView hamstring;
+	@FXML ImageView hofteIn;
+	
 	private MuscleImage muscleMan;
+	
+	private List<ImageView> muscles ;
 	
 	// Managercontroller for kommunikasjon mellom controllers
 	public ManagerController managerController;
@@ -40,13 +52,17 @@ public class StrengthTabController {
 	// Setter managerController
 	public void init(ManagerController managerController) {
 		this.managerController = managerController;
-		setMuscleMan();
+		muscles = Arrays.asList(underarmer, abs, 
+				triceps, teres, postDelts, obliques, lats, frontDelts, erector,
+				calves, biceps, quads, rumpe, traps, hofteUt, bryst, hamstring, hofteIn);
 	}
 	
-	
-	private void setMuscleMan() {
+	//Setter musklene til tilfeldig grad av rødfarge
+	public void random() {
 		Random r = new Random();
-		underarmer.setOpacity(1.0);
+		for (ImageView muscle : muscles){
+			muscle.setOpacity(r.nextDouble());
+		}
 	}
 	
 	// Setter user som skrives i sokefeltet
