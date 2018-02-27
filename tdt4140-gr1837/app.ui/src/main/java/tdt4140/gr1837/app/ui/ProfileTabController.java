18d5abc96cf85fdbc50e8614e6e7f76bdd0003d5
lurelars <larsmk96@gmail.com>
2018-02-27 12:09:22 +0100

    #8 Ramme av profilside med liste over øvelser

--- tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/ProfileTabController.java
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/ProfileTabController.java
@@ -13,10 +13,12 @@ public class ProfileTabController {
 	
 	// ManagerController for kommunikasjon med andre controllers
 	public ManagerController managerController;
+	private SessionListController sessionListController = new SessionListController();
 	
 	// Initialiserer managerController for a fa riktig controller pga fx:include
 	public void init(ManagerController managerController) {
 		this.managerController = managerController;
+		
 	}
 
 	// Setter user som skrives i sokefeltet
---
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/SessionListController.java
@@ -0,0 +1,15 @@
+package tdt4140.gr1837.app.ui;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+
+public class SessionListController {
+	
+	@FXML
+	ListView SessionList;
+	
+	@FXML
+	TableView ExerciseList;
+	
+	
+}
