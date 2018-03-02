4a2518b57e94991ef2cc6c7fd6dfa394e10fe7d6
lurelars <larsmk96@gmail.com>
2018-03-02 10:07:37 +0100

    Forsøk på å få informasjon fra database til tabell, #8

--- tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/ProfileTabController.java
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/ProfileTabController.java
@@ -6,7 +6,11 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableColumn;
 import javafx.scene.text.Text;
+import tdt4140.gr1837.app.core.Exercise;
 import tdt4140.gr1837.app.core.User;
 
 public class ProfileTabController {
@@ -15,6 +19,12 @@ public class ProfileTabController {
 	@FXML Tab profileTab;
 	@FXML Text clientName;
 	@FXML ListView<User> TrainingList1;
+	@FXML TableView<Exercise> ExerciseList;
+	@FXML TableColumn<Exercise, String> type;
+	@FXML TableColumn<Exercise, String> notat;
+	@FXML TableColumn<Exercise, Integer> sett;
+	@FXML TableColumn<Exercise, Integer> repetisjoner;
+	@FXML TableColumn<Exercise, Integer> vekt;
 	
 	// ManagerController for kommunikasjon med andre controllers
 	public ManagerController managerController;
@@ -30,6 +40,34 @@ public class ProfileTabController {
 	public void setUser(User user, List<User> users) {
 		clientName.setText(user.getName());
 		TrainingList1.setItems(FXCollections.observableArrayList(users));
+	}
+	
+	// Users i denne metoden må endres til øvelser, bruker users for å teste
+	private void addTableView(List<User> users) {
+		// TableView<Exercise> ExerciseList = new TableView<Exercise>();
+		
+		type.setCellValueFactory(new PropertyValueFactory<>("type"));
+		notat.setCellValueFactory(new PropertyValueFactory<>("notat"));
+		sett.setCellValueFactory(new PropertyValueFactory<>("sett"));
+		repetisjoner.setCellValueFactory(new PropertyValueFactory<>("repetisjoner"));
+		vekt.setCellValueFactory(new PropertyValueFactory<>("vekt"));
+		
+		ExerciseList.setItems(FXCollections.observableArrayList());
+		
+		
+		
+		/*TableColumn type = new TableColumn("Type");
+		type.setCellValueFactory(new PropertyValueFactory<Exercise, String>("type"));
+		
+		TableColumn sett = new TableColumn("Sett");
+		sett.setCellValueFactory(new PropertyValueFactory<Exercise, String>("sett"));
+		
+		TableColumn repetisjoner = new TableColumn("Repetisjoner");
+		type.setCellValueFactory(new PropertyValueFactory<Exercise, String>("Type"));
+		
+		TableColumn vekt = new TableColumn("Vekt");
+		vekt.setCellValueFactory(new PropertyValueFactory<Exercise, String>("vekt"));*/
+		
 	}
 		
 
