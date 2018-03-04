3a417eb18900fc6ca2657c65fd68e1c0f4316e63
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-03-04 16:12:39 +0100

    #6 småendringer på søketaben + logo til vinduet

--- tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/ManagerLauncher.java
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/ManagerLauncher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 // Klasse for a kjore manager.fxml
@@ -17,7 +18,10 @@ public class ManagerLauncher extends Application {
         
         stage.setTitle("Treningsdagbok");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("file:../../../img/logo2.png"));
         stage.show();
+        
+        
     }
 
     public static void main(String[] args) {
