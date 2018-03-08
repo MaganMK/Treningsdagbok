1b26d3639c035a76043d98604b181fc206bc450b
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-03-08 16:46:45 +0100

    #20 Legge inn testdata til graf og klikkbare noder med popupvindu

--- tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StrengthTabController.java
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StrengthTabController.java
@@ -8,18 +8,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import org.controlsfx.control.CheckListView;
+import org.controlsfx.control.PopOver;
+
 import com.jfoenix.controls.JFXListView;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import tdt4140.gr1837.app.core.Exercise;
 import tdt4140.gr1837.app.core.MuscleImage;
 import tdt4140.gr1837.app.core.SQLConnector;
@@ -64,6 +76,16 @@ public class StrengthTabController {
 	@FXML ImageView hamstring;
 	@FXML ImageView hofteIn;
 	
+	//Checkboxes
+	@FXML CheckListView<String> checkList;
+		
+	//Radiobuttons
+	@FXML ToggleGroup dataRepresentation;
+	@FXML RadioButton rmRadioButton, vektvolumRadioButton;
+		
+	//Graf
+	@FXML LineChart<Number,Number> strengthChart;
+	
 	private MuscleImage muscleMan;
 	
 	private Map<String, ImageView> muscles = new HashMap<>();
@@ -74,6 +96,7 @@ public class StrengthTabController {
 	// Setter managerController
 	public void init(ManagerController managerController) {
 		this.managerController = managerController;
+		setTestData();
 	}
 	
 	//Setter musklene til grad av rÃ¸dfarge
@@ -153,5 +176,58 @@ public class StrengthTabController {
 	// Setter notatfeltet
 	private void addNoteView(String note) {
 		this.note.setText(note);
+	}
+	
+	//Setter inn testdata i grafen som erstatning for database
+	public void setTestData() {
+		strengthChart.getXAxis().setLabel("Økt");
+		strengthChart.getYAxis().setLabel("Vekt");
+		
+		//Legger en series inn i charten (feks en sekvens for en øvelse)
+		XYChart.Series<Number, Number> series1 = new XYChart.Series<>();
+	    series1.setName("Benkpress");
+	    series1.getData().add(new XYChart.Data<>(1, 70));
+	    series1.getData().add(new XYChart.Data<>(2, 72.4));
+	    series1.getData().add(new XYChart.Data<>(3, 80));
+	    series1.getData().add(new XYChart.Data<>(4, 75));
+	    series1.getData().add(new XYChart.Data<>(5, 70));
+	    series1.getData().add(new XYChart.Data<>(6, 82));
+	    strengthChart.getData().add(series1);
+	    setSeriesNodeControls(series1);
+	}
+	
+	// Gjor nodene til en serie i grafen klikkbare
+	public void setSeriesNodeControls(XYChart.Series<Number, Number> series) {
+	    for (int i = 0; i <	series.getData().size(); i++) {
+			final int j = i;
+			// aapner popup-vindu til noden som trykkes paa
+			series.getData().get(i).getNode().setOnMousePressed(e -> {
+				 
+				 PopOver pop = new PopOver();
+				 
+				 Bounds boundsInScreen = series.getData().get(j).getNode().localToScreen(series.getData().get(j).getNode().getBoundsInLocal());
+				 Double y = boundsInScreen.getMaxY() - 44;
+				 Double x = boundsInScreen.getMinX() + 4;
+				 
+				 
+			     pop.setX(x);
+			     pop.setY(y);
+			     
+			     pop.setMinSize(300, 150);
+				 pop.show((Stage)series.getData().get(j).getNode().getScene().getWindow());
+			});
+			
+			//Setter cursoren til hand ved hover
+			series.getData().get(i).getNode().setOnMouseMoved(e -> {
+				Scene scene = series.getData().get(j).getNode().getScene();
+				scene.setCursor(Cursor.HAND);
+			});
+			
+			//Setter cursoren til default når den er ute av noden
+			series.getData().get(i).getNode().setOnMouseExited(e -> {
+				Scene scene = series.getData().get(j).getNode().getScene();
+				scene.setCursor(Cursor.DEFAULT);
+			});
+		}
 	}
 }
