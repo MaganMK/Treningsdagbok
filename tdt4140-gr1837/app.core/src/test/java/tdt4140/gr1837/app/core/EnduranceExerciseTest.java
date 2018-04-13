8649c4e30e12d1b949ac80047fa9d3e63829c0bc
MaganMK <trebil77>
2018-04-13 15:52:11 +0200

    [#30] Lagt til tester i core og UI for kondisjonsgrafen

---
+++ tdt4140-gr1837/app.core/src/test/java/tdt4140/gr1837/app/core/EnduranceExerciseTest.java
@@ -0,0 +1,45 @@
+package tdt4140.gr1837.app.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Time;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EnduranceExerciseTest {
+
+	EnduranceExercise exercise = new EnduranceExercise("5,0km løp", "Tuuungt", 5000, new Time(50000));
+	
+	
+	@Test
+	public void testExerciseGetters() {
+		assertTrue("5,0km løp".equals(exercise.getName()));
+		assertTrue("Tuuungt".equals(exercise.getNote()));
+		assertTrue(5000 == exercise.getDistance());
+	}
+	
+
+	
+	
+	@Test
+	public void testGetAverageSpeed() {
+		double averageSpeed = exercise.getAverageSpeed();
+		assertTrue(4.931506849315069 == averageSpeed);
+	}
+	
+
+	@Test
+	public void testGetDistanceRepresantation() {
+		double distance = exercise.getDistanceRepresantation();
+		assertTrue(5.0 == distance);
+	}
+
+	@Test
+	public void testGetTimeRepresentation() {
+		double time = exercise.getDistanceRepresantation();
+		assertTrue(5.0 == time);
+	}
+	
+}
