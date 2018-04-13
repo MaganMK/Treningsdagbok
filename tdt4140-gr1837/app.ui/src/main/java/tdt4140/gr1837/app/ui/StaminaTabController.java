f274aa3d282a40872d7c56501d7eb809774e5bb8
MaganMK <trebil77>
2018-04-13 15:26:35 +0200

    [#30 #28] Fikset merge-conflicts etter rebase med master

--- tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StaminaTabController.java
+++ tdt4140-gr1837/app.ui/src/main/java/tdt4140/gr1837/app/ui/StaminaTabController.java
@@ -1,7 +1,13 @@
 package tdt4140.gr1837.app.ui;
 
+import java.util.Map;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.text.Text;
+import tdt4140.gr1837.app.core.SQLConnector;
 import tdt4140.gr1837.app.core.User;
 
 // Staminatab for graf osv. til utholdenhetsovelser
@@ -24,6 +30,26 @@ public class StaminaTabController {
 	public void setUser(User user) {
 		//clientName.setText(user.getName());
 		staminaChartController.setUser(user);
+		updatePieChart(user);
 	}
+	
+	@FXML PieChart piechart;
+	// Oppdaterer pichartet i henhold til brukerens treningsdata
+	public void updatePieChart(User user) {
+		Map<String, Integer> enduranceDistribution = SQLConnector.getEnduranceDistribution(user.getId());
+		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+		if (!enduranceDistribution.isEmpty()){
+			for (String key : enduranceDistribution.keySet()) {
+				pieChartData.add(new PieChart.Data(key, enduranceDistribution.get(key)));
+			}
+			this.piechart.setTitle("Tid brukt på ulike øvelser");
+		}
+		else {
+			this.piechart.setTitle("Ingen utholdenhetsøvelser registrert");
+		}
+		this.piechart.setData(pieChartData);
+		this.piechart.setLabelsVisible(false);
+	}
+
 
 }
